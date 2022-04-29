fd2fa4a4ebf5fd89268066ff993b0a93b5eff76a
irama <irama.chaouch@epitech.eu>
2022-04-30 02:15:06 +0900

    [MOD] Modified main.py -- init the interface, create score, set the score, up the score and display it

--- interface_module/main.py
+++ interface_module/main.py
@@ -1,13 +1,18 @@
+from curses import window
 import pygame
+from pygame.locals import *
+from Interface import Interface
 
-background_colour = (000000)
+
 (width, height) = (1000, 800)
 screen = pygame.display.set_mode((width, height))
+pygame.font.init()
 pygame.display.set_caption('Interface')
-screen.fill(background_colour)
-pygame.display.flip()
 running = True
+interface = Interface(screen)
 while running:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
+    interface.display()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.update()
